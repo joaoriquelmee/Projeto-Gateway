cfa81c9bad99197377593df8d7db0a6641d9bfc0
João Riquelme <149601492+joaoriquelmee@users.noreply.github.com>
2024-12-25 19:22:17 -0300

    final update

---
+++ código/index.js
@@ -0,0 +1,9 @@
+let img1 = document.getElementById('imgtestejs')
+
+function alterar() {
+    img1.src = "../imagens/testeazul.png"
+}
+
+function voltar() {
+    img1.src = "../imagens/padraored.png"
+}
